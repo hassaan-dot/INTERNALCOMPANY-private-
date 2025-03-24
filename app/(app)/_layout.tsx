f79f2222797ab06b615da03705f32cb375a6b1b0
haseeb irfan <haseeb_irfan1368@hotmail.com>
2025-03-24 22:43:27 +0500

    layout fix

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,17 +1,9 @@
 import Sidebar from "@/components/drawer/sidebar";
 import { useAuthStore } from "@/store/useAuthStore";
 import { Redirect } from "expo-router";
-import Bottomtabs from "../../components/Bottom/Bottom";
-import { Platform } from "react-native";
+
 export default function AuthLayout() {
-  const ismobileView = Platform.OS == "ios";
   const { token } = useAuthStore();
-  if (token) return <Redirect href={"/(auth)/login"} />;
-  if (!ismobileView) return <Sidebar></Sidebar>;
-  if (!ismobileView) {
-    return <Redirect href={"/(app)/dashboard"} />;
-  }
-  if (ismobileView) {
-    return <Redirect href={"/(tabs)/dashboard"} />;
-  }
+  if (!token) return <Redirect href={"/(auth)/login"} />;
+  return <Sidebar />;
 }
